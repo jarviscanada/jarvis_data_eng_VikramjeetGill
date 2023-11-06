4b190e94f9e4654789a9f012d526fbd7ec8f386f
Vikramjeet Gill <ruthlesspanda26@gmail.com>
2023-11-06 17:01:23 +0000

    finished host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -18,12 +18,13 @@ cpu_number=$(lscpu | grep "CPU(s):" | awk '{print $2}' | awk '!/node0/')
 cpu_architecture=$(lscpu | grep "Architecture" | awk '{print $2}')
 cpu_model=$(lscpu | grep "Model name" | awk -F': ' '{print $2}' | awk '{$1=$1;print}')
 cpu_mhz=$(lscpu | grep "CPU MHz" | awk '{print $3}')
-l2_cache=$(lscpu | grep "L2 cache" | awk '{print $3}')
+l2_cache=$(lscpu | grep "L2 cache" | awk '{print $3}' | sed 's/[^0-9]//g')
 total_mem=$(vmstat --unit M | tail -1 | awk '{print $4}')
 timestamp=$(vmstat -t | awk '{print $18, $19}' | tail -n1)
-
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
-insert_stmt="INSERT INTO host_info(id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem)    VALUES('$host_id', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$timestamp', '$total_mem')";
+# I have taken out the id column from the insert command. Not sure if this will work.
+# Seemes to be having a problem generating new ids since 1 is already in use. idk why
+# it isn't just moving on to 2 or 3 etc...?
+insert_stmt="INSERT INTO host_info(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, timestamp, total_mem)    VALUES('$hostname','$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$timestamp', '$total_mem')";
 
 export PGPASSWORD=$psql_password
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
