fd491e4d9aa06642be1df86b724403cdaf145a1a
Vikramjeet Gill <45413532+Vikramjeet-Gill@users.noreply.github.com>
2024-07-12 15:27:23 -0400

    Made some minor changes to strings in the mapping tags

--- mongo/insurance-mongo/src/main/java/ca/jrvs/insurance_api/controller/PersonController.java
+++ mongo/insurance-mongo/src/main/java/ca/jrvs/insurance_api/controller/PersonController.java
@@ -14,17 +14,17 @@ import java.util.Optional;
 @RestController
 @RequestMapping("insurance_api")
 public class PersonController {
-
-    @Autowired // Might not need this tag
+    
     private final PersonService service;
 
+    @Autowired
     public PersonController(PersonService service) {
         this.service = service;
     }
 
     // This method saves an individual person to the collection
 
-    @PostMapping("person")
+    @PostMapping("/person")
     @ResponseStatus(HttpStatus.CREATED)
     public void postPerson(@RequestBody Person person) {
         service.save(person);
@@ -32,15 +32,15 @@ public class PersonController {
 
     // This method retrieves a list of everyone in the people collection
 
-    @GetMapping("people")
+    @GetMapping("/people")
     public List<Person> getPeople() {
         return service.findAll();
     }
 
     // This method retrieves a person from the people collection based on the id
 
-    @GetMapping("person/{id}")
-    public ResponseEntity<Person> getPerson(@PathVariable ObjectId id) {
+    @GetMapping("/person/{id}")
+    public ResponseEntity<Person> getPerson(@PathVariable String id) {
         Optional<Person> o = service.findOne(id);
         if (o.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -50,21 +50,21 @@ public class PersonController {
 
     // This method saves all the people from a list of people to the people collection
 
-    @PostMapping("people")
+    @PostMapping("/people")
     public void postPeople(@RequestBody List<Person> people) {
         service.saveAll(people);
     }
 
     // This method returns a list of people from the collection people whose id match one of the ids in the provided list
 
-    @GetMapping("people/{ids}")
+    @GetMapping("/people/{ids}")
     public List<Person> getPeopleByIds(@PathVariable List<ObjectId> ids) {
         return service.findAll(ids);
     }
 
     // This method deletes a person from the collection people whose id matches the provided id
 
-    @DeleteMapping("person/{id}")
+    @DeleteMapping("/person/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePerson(@PathVariable ObjectId id) {
         service.delete(id);
@@ -72,7 +72,7 @@ public class PersonController {
 
     // This method deletes people from the collection people whose id match one of the ids provided in the list
 
-    @DeleteMapping("people")
+    @DeleteMapping("/people")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePeople(@RequestBody List<ObjectId> ids) {
         service.delete(ids);
@@ -80,7 +80,7 @@ public class PersonController {
 
     // This method deletes all people from the collection people
 
-    @DeleteMapping("people/all")
+    @DeleteMapping("/people/all")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteAllPeople() {
         service.deleteAll();
@@ -88,7 +88,7 @@ public class PersonController {
 
     // This method updates a person
 
-    @PutMapping("person")
+    @PutMapping("/person")
     @ResponseStatus(HttpStatus.OK)
     public void updatePerson(@RequestBody Person person) {
         service.update(person);
@@ -96,7 +96,7 @@ public class PersonController {
 
     // This method updates a list of people in the collection people
 
-    @PutMapping("people")
+    @PutMapping("/people")
     @ResponseStatus(HttpStatus.OK)
     public void updatePeople(@RequestBody List<Person> people) {
         service.update(people);
@@ -104,21 +104,21 @@ public class PersonController {
 
     // This method returns a count of the total number of people in the collection people
 
-    @GetMapping("people/count")
+    @GetMapping("/people/count")
     public long countPeople() {
         return service.count();
     }
 
     // This method returns the average age of all people in the collection people
 
-    @GetMapping("people/average-age")
+    @GetMapping("/people/average-age")
     public double getAverageAge() {
         return service.getAverageAge();
     }
 
     // This method returns the largest number of cars associated with any person in the collection people.
 
-    @GetMapping("people/max-cars")
+    @GetMapping("/people/max-cars")
     public int getMaxCars() {
         return service.getMaxCars();
     }
